aaccbc634a8ef7676503749d50eac9ec0ac044a7
kalyanigadade <gadadekalyani1@gmail.com>
2022-04-21 15:59:34 +0530

    My 1st Addition program

---
+++ aadition.py
@@ -0,0 +1,13 @@
+a=27
+b=49
+c=a+b
+print(c)
+spam='hello'
+print(spam)
+print("how are you")
+for i in range(0,10,2):
+    print(i)
+
+print("corrected code output")
+for i in range(2,22,2):
+    print(i)
