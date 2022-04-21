445b51d37b01e702ec0d8ac61bfef5ad708a5503
kalyanigadade <gadadekalyani1@gmail.com>
2022-04-21 17:08:49 +0530

    My Else Statements

---
+++ Else statements.py
@@ -0,0 +1,12 @@
+name = "Alice"
+if name == "Bob":
+    print("Hi,Bob.")
+else:
+    print("Hello,Alice")
+name = "Harshada"
+if name == "Kalyani":
+    print("Hi.")
+else:
+    print("Hi,Harshada.")
+
+
