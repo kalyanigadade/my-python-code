583178fc09ede71ff067fd5a01a57960ad88efd7
kalyanigadade <gadadekalyani1@gmail.com>
2022-04-21 16:55:48 +0530

    My If Statements.

---
+++ If Statments.py
@@ -0,0 +1,7 @@
+name = "Alice"
+if name == "Alice":
+    print("Hi Alice")
+name = 'Priti'
+if name == "Priti":
+    print("Hi Priti,How are you?")
+
---
+++ Mathopretion.py
@@ -0,0 +1,6 @@
+a= int(input("Enter 1st number:"))
+b= int(input("Enter 2nd number:"))
+c=a+b
+print("Addition of 2 number is ",c)
+c=a-b
+print("Subtraction of 2 number is ",c)
