bdc7181b19c258686df942632ac2465bfc2f5b1e
kalyanigadade <gadadekalyani1@gmail.com>
2022-04-21 20:28:49 +0530

    Tables and Even

---
+++ Even.py
@@ -0,0 +1,20 @@
+for i in range(2,102,2):
+    print (i)
+for i in range(2,22,2):
+    print (i)
+for i in range(3,32,3):
+    print(i)
+for i in range(4,42,4):
+    print(i)
+for i in range(5,52,5):
+    print(i)
+for i in range(6,62,6):
+    print(i)
+for i in range(7,72,7):
+    print(i)
+for i in range(8,82,8):
+    print(i)
+for i in range(9,92,9):
+    print(i)
+for i in range(10,110,10):
+    print(i)
