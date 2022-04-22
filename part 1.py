541a766fe5162d3d9867987446c2b32b403c0386
kalyanigadade <gadadekalyani1@gmail.com>
2022-04-22 18:10:13 +0530

    Hearts

---
+++ part 1.py
@@ -0,0 +1,127 @@
+for row in range(6):
+    for col in range(7):
+        if (row==0 and col%3!=0) or (row==1 and col%3==0) or (row-col==2) or (row+col==8):
+            print("*",end="")
+        else:
+            print(" ",end="")
+    print()
+for row in range(6):
+    for col in range(7):
+        if (row==0 and col%3!=0) or (row==1 and col%3==0) or (row-col==2) or (row+col==8):
+            print("@",end="")
+        else:
+            print(" ",end="")
+    print()
+for row in range(6):
+    for col in range(7):
+        if (row==0 and col%3!=0) or (row==1 and col%3==0) or (row-col==2) or (row+col==8):
+            print("#",end="")
+        else:
+            print(" ",end="")
+    print()
+for row in range(6):
+    for col in range(7):
+        if (row==0 and col%3!=0) or (row==1 and col%3==0) or (row-col==2) or (row+col==8):
+            print("!",end="")
+        else:
+            print(" ",end="")
+    print()
+for row in range(6):
+    for col in range(7):
+        if (row==0 and col%3!=0) or (row==1 and col%3==0) or (row-col==2) or (row+col==8):
+            print("$",end="")
+        else:
+            print(" ",end="")
+    print()
+for row in range(6):
+    for col in range(7):
+        if (row==0 and col%3!=0) or (row==1 and col%3==0) or (row-col==2) or (row+col==8):
+            print("%",end="")
+        else:
+            print(" ",end="")
+    print()
+for row in range(6):
+    for col in range(7):
+        if (row==0 and col%3!=0) or (row==1 and col%3==0) or (row-col==2) or (row+col==8):
+            print("^",end="")
+        else:
+            print(" ",end="")
+    print()
+for row in range(6):
+    for col in range(7):
+        if (row==0 and col%3!=0) or (row==1 and col%3==0) or (row-col==2) or (row+col==8):
+            print("&",end="")
+        else:
+            print(" ",end="")
+    print()
+for row in range(6):
+    for col in range(7):
+        if (row==0 and col%3!=0) or (row==1 and col%3==0) or (row-col==2) or (row+col==8):
+            print("-",end="")
+        else:
+            print(" ",end="")
+    print()
+for row in range(6):
+    for col in range(7):
+        if (row==0 and col%3!=0) or (row==1 and col%3==0) or (row-col==2) or (row+col==8):
+            print("+",end="")
+        else:
+            print(" ",end="")
+    print()
+for row in range(6):
+    for col in range(7):
+        if (row==0 and col%3!=0) or (row==1 and col%3==0) or (row-col==2) or (row+col==8):
+            print("=",end="")
+        else:
+            print(" ",end="")
+    print()
+for row in range(6):
+    for col in range(7):
+        if (row==0 and col%3!=0) or (row==1 and col%3==0) or (row-col==2) or (row+col==8):
+            print("|",end="")
+        else:
+            print(" ",end="")
+    print()
+for row in range(6):
+    for col in range(7):
+        if (row==0 and col%3!=0) or (row==1 and col%3==0) or (row-col==2) or (row+col==8):
+            print("9",end="")
+        else:
+            print(" ",end="")
+    print()
+for row in range(6):
+    for col in range(7):
+        if (row==0 and col%3!=0) or (row==1 and col%3==0) or (row-col==2) or (row+col==8):
+            print('_',end="")
+        else:
+            print(" ",end="")
+    print()
+
+for row in range(6):
+    for col in range(7):
+        if (row==0 and col%3!=0) or (row==1 and col%3==0) or (row-col==2) or (row+col==8):
+            print("~",end="")
+        else:
+            print(" ",end="")
+    print()
+for row in range(6):
+    for col in range(7):
+        if (row==0 and col%3!=0) or (row==1 and col%3==0) or (row-col==2) or (row+col==8):
+            print("/",end="")
+        else:
+            print(" ",end="")
+    print()
+for row in range(6):
+    for col in range(7):
+        if (row==0 and col%3!=0) or (row==1 and col%3==0) or (row-col==2) or (row+col==8):
+            print("S",end="")
+        else:
+            print(" ",end="")
+    print()
+for row in range(6):
+    for col in range(7):
+        if (row==0 and col%3!=0) or (row==1 and col%3==0) or (row-col==2) or (row+col==8):
+            print("",end="")
+        else:
+            print(" ",end="")
+    print()
