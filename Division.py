7015e21e06a524f26795d6a7d2b56bc7a7f5b1d4
kalyanigadade <gadadekalyani1@gmail.com>
2022-04-23 00:06:43 +0530

    opreaters

---
+++ Division.py
@@ -0,0 +1,91 @@
+a=int(input("Enter 1st number:"))
+b=int(input("Enter 2nd number:"))
+c=a/b
+print("Answer is ",c)
+a=int(input("Enter 1st number:"))
+b=int(input("Enter 2nd number:"))
+c=a/b
+print("Answer is ",c)
+a=int(input("Enter 1st number:"))
+b=int(input("Enter 2nd number:"))
+c=a/b
+print("Answer is ",c)
+a=int(input("Enter 1st number:"))
+b=int(input("Enter 2nd number:"))
+c=a/b
+print("Answer is ",c)
+a=int(input("Enter 1st number:"))
+b=int(input("Enter 2nd number:"))
+c=a/b
+print("Answer is ",c)
+a=int(input("Enter 1st number:"))
+b=int(input("Enter 2nd number:"))
+c=a/b
+print("Answer is ",c)
+a=int(input("Enter 1st number:"))
+b=int(input("Enter 2nd number:"))
+c=a*b
+print("Answer is ",c)
+a=int(input("Enter 1st number:"))
+b=int(input("Enter 2nd number:"))
+c=a*b
+print("Answer is ",c)
+a=int(input("Enter 1st number:"))
+b=int(input("Enter 2nd number:"))
+c=a*b
+print("Answer is ",c)
+a=int(input("Enter 1st number:"))
+b=int(input("Enter 2nd number:"))
+c=a*b
+print("Answer is ",c)
+a=int(input("Enter 1st number:"))
+b=int(input("Enter 2nd number:"))
+c=a*b
+print("Answer is ",c)
+a=int(input("Enter 1st number:"))
+b=int(input("Enter 2nd number:"))
+c=a+b
+print("Answer is ",c)
+a=int(input("Enter 1st number:"))
+b=int(input("Enter 2nd number:"))
+c=a+b
+print("Answer is ",c)
+a=int(input("Enter 1st number:"))
+b=int(input("Enter 2nd number:"))
+c=a+b
+print("Answer is ",c)
+a=int(input("Enter 1st number:"))
+b=int(input("Enter 2nd number:"))
+c=a+b
+print("Answer is ",c)
+a=int(input("Enter 1st number:"))
+b=int(input("Enter 2nd number:"))
+c=a+b
+print("Answer is ",c)
+a=int(input("Enter 1st number:"))
+b=int(input("Enter 2nd number:"))
+c=a-b
+print("Answer is ",c)
+a=int(input("Enter 1st number:"))
+b=int(input("Enter 2nd number:"))
+c=a-b
+print("Answer is ",c)
+a=int(input("Enter 1st number:"))
+b=int(input("Enter 2nd number:"))
+c=a-b
+print("Answer is ",c)
+a=int(input("Enter 1st number:"))
+b=int(input("Enter 2nd number:"))
+c=a-b
+print("Answer is ",c)
+a=int(input("Enter 1st number:"))
+b=int(input("Enter 2nd number:"))
+c=a-b
+print("Answer is ",c)
+a=int(input("Enter 1st number:"))
+b=int(input("Enter 2nd number:"))
+c=a-b
+print("Answer is ",c)
+
+
+
