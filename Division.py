834833c01cae4969ba0b31a49f01e730e9e311cd
kalyanigadade <gadadekalyani1@gmail.com>
2022-04-23 10:09:32 +0530

    Output

--- Division.py
+++ Division.py
@@ -1,91 +1,41 @@
 a=int(input("Enter 1st number:"))
 b=int(input("Enter 2nd number:"))
-c=a/b
-print("Answer is ",c)
-a=int(input("Enter 1st number:"))
-b=int(input("Enter 2nd number:"))
-c=a/b
-print("Answer is ",c)
-a=int(input("Enter 1st number:"))
-b=int(input("Enter 2nd number:"))
-c=a/b
-print("Answer is ",c)
-a=int(input("Enter 1st number:"))
-b=int(input("Enter 2nd number:"))
-c=a/b
-print("Answer is ",c)
-a=int(input("Enter 1st number:"))
-b=int(input("Enter 2nd number:"))
-c=a/b
-print("Answer is ",c)
-a=int(input("Enter 1st number:"))
-b=int(input("Enter 2nd number:"))
-c=a/b
-print("Answer is ",c)
-a=int(input("Enter 1st number:"))
-b=int(input("Enter 2nd number:"))
-c=a*b
-print("Answer is ",c)
-a=int(input("Enter 1st number:"))
-b=int(input("Enter 2nd number:"))
-c=a*b
-print("Answer is ",c)
-a=int(input("Enter 1st number:"))
-b=int(input("Enter 2nd number:"))
-c=a*b
-print("Answer is ",c)
-a=int(input("Enter 1st number:"))
-b=int(input("Enter 2nd number:"))
-c=a*b
-print("Answer is ",c)
-a=int(input("Enter 1st number:"))
-b=int(input("Enter 2nd number:"))
-c=a*b
-print("Answer is ",c)
-a=int(input("Enter 1st number:"))
-b=int(input("Enter 2nd number:"))
 c=a+b
-print("Answer is ",c)
-a=int(input("Enter 1st number:"))
-b=int(input("Enter 2nd number:"))
-c=a+b
-print("Answer is ",c)
-a=int(input("Enter 1st number:"))
-b=int(input("Enter 2nd number:"))
-c=a+b
-print("Answer is ",c)
-a=int(input("Enter 1st number:"))
-b=int(input("Enter 2nd number:"))
-c=a+b
-print("Answer is ",c)
-a=int(input("Enter 1st number:"))
-b=int(input("Enter 2nd number:"))
-c=a+b
-print("Answer is ",c)
-a=int(input("Enter 1st number:"))
-b=int(input("Enter 2nd number:"))
-c=a-b
-print("Answer is ",c)
-a=int(input("Enter 1st number:"))
-b=int(input("Enter 2nd number:"))
-c=a-b
-print("Answer is ",c)
-a=int(input("Enter 1st number:"))
-b=int(input("Enter 2nd number:"))
-c=a-b
-print("Answer is ",c)
-a=int(input("Enter 1st number:"))
-b=int(input("Enter 2nd number:"))
-c=a-b
-print("Answer is ",c)
-a=int(input("Enter 1st number:"))
-b=int(input("Enter 2nd number:"))
-c=a-b
-print("Answer is ",c)
-a=int(input("Enter 1st number:"))
-b=int(input("Enter 2nd number:"))
-c=a-b
-print("Answer is ",c)
+print("Answer is",c)
 
+a=int(input("Enter 1st number:"))
+b=int(input("Enter 2nd number:"))
+c=a+b
+print("Answer is",c)
+
+a=int(input("Enter 1st number:"))
+b=int(input("Enter 2nd number:"))
+c=a*b
+print("Answer is",c)
+
+a=int(input("Enter 1st number:"))
+b=int(input("Enter 2nd number:"))
+c=a*b
+print("Answer is",c)
+
+a=int(input("Enter 1st number:"))
+b=int(input("Enter 2nd number:"))
+c=a/b
+print("Answer is",c)
+
+a=int(input("Enter 1st number:"))
+b=int(input("Enter 2nd number:"))
+c=a/b
+print("Answer is",c)
+
+a=int(input("Enter 1st number:"))
+b=int(input("Enter 2nd number:"))
+c=a-b
+print("Answer is",c)
+
+a=int(input("Enter 1st number:"))
+b=int(input("Enter 2nd number:"))
+c=a-b
+print("Answer is",c)
 
 
