c39c53196eddb1639f3f939ba6b6633ecaddff18
kalyanigadade <gadadekalyani1@gmail.com>
2022-04-24 12:42:01 +0530

    Output of even and odd

---
+++ Revision.py
@@ -0,0 +1,25 @@
+num = int(input("Enter a number: "))
+mod = num % 2
+if mod > 0:
+    print("This is an odd number.")
+else:
+    print("This is an even number.")
+
+num = int(input("Enter a number: "))
+mod = num % 2
+if mod > 0:
+    print("This is an odd number.")
+else:
+    print("This is an even number.")
+num = int(input("Enter a number: "))
+mod = num % 2
+if mod > 0:
+    print("This is an odd number.")
+else:
+    print("This is an even number.")
+num=int(input("Enter a number:"))
+mod=num%2
+if mod > 0:
+    print("This is a odd number.")
+else:
+    print("This is a even number:")
